05c7656b81beacd8d6bf4468e1f584a2c022b04f
Fzferrara <fzferrara@gmail.com>
2024-12-06 17:41:45 +0100

    A word counter has been created based on an algorithm practice in which pseudocode and flowcharts were used.

---
+++ letters_count.py
@@ -0,0 +1,20 @@
+
+
+# Inicio del programa:
+#     //Paso 1: Solicitar entrada al usuario
+#     Mostrar mensaje: "Por favor, ingrese una palabra: "
+#     Definir la variable palabra_ingresada con la lectura de la entrada.
+
+palabra_ingresada = input("Por favor, ingrese una palabra: ")
+
+#     //Paso 2: Contar cantidad de letras.
+#     Definir la variable cantidad_letras y asignarle la longitud de palabra_ingresada
+
+cantidad_letras = len(palabra_ingresada)
+
+
+#     Mostrar mensaje: "La palabra ingresada tiene" , cantidad_letras, "letras".
+
+print("La palabra ingresada tiene", cantidad_letras, "letras" )
+
+# Fin de Programa.
